af6aa4248526639b544b1b98f572676d7f41be95
ohmatheus <max.marechal@gmail.com>
2025-12-19 08:31:00 +0100

    Added base simulation data

---
+++ engine/include/SimulationBuffer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+namespace FluidNet
+{
+
+struct SimulationBuffer
+{
+    std::vector<float> velocityX;
+    std::vector<float> velocityY;
+    std::vector<float> density;
+
+    int gridResolution{128};
+    uint64_t frameNumber{0};
+    double timestamp{0.0};
+    bool isDirty{false};
+
+    void allocate(int resolution);
+};
+
+}
