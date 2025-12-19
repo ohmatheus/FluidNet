4ba0a7b073b9b456cbf433789cd64e3ca7c91692
ohmatheus <max.marechal@gmail.com>
2025-12-19 11:00:02 +0100

    Fixed 'restart' not updating buffer's data to 0

--- engine/src/SimulationBuffer.cpp
+++ engine/src/SimulationBuffer.cpp
@@ -12,6 +12,10 @@ void SimulationBuffer::allocate(int resolution)
     velocityY.resize(size, 0.0f);
     density.resize(size, 0.0f);
 
+    std::fill(velocityX.begin(), velocityX.end(), 0.0f);
+    std::fill(velocityY.begin(), velocityY.end(), 0.0f);
+    std::fill(density.begin(), density.end(), 0.0f);
+
     frameNumber = 0;
     timestamp = 0.0;
     isDirty = true;
