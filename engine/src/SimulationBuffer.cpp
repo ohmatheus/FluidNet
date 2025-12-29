8da575d8a519d54a7bcf930aba298f6d8a8ab8f1
ohmatheus <max.marechal@gmail.com>
2025-12-29 10:46:52 +0100

    Added collider mask to model input in c++ engine.

--- engine/src/SimulationBuffer.cpp
+++ engine/src/SimulationBuffer.cpp
@@ -11,12 +11,18 @@ void SimulationBuffer::allocate(int resolution)
     density.resize(size, 0.0f);
     velocityX.resize(size, 0.0f);
     velocityY.resize(size, 0.0f);
+
+    // todo: move those 2 as user input
     emitterMask.resize(size, 0.0f);
+    colliderMask.resize(size, 0.0f);
 
     std::fill(density.begin(), density.end(), 0.0f);
     std::fill(velocityX.begin(), velocityX.end(), 0.0f);
     std::fill(velocityY.begin(), velocityY.end(), 0.0f);
+
+    // todo: move those 2 as user input
     std::fill(emitterMask.begin(), emitterMask.end(), 0.0f);
+    std::fill(colliderMask.begin(), colliderMask.end(), 0.0f);
 
     frameNumber = 0;
     timestamp = 0.0;
